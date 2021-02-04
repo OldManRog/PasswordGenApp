3f2c86736c4b93d78afba738f1ac31d53f813cfa
Rogerr Oliva <Roliva75@yahoo.com>
2021-02-04 00:00:40 -0600

    finished project!

--- main.js
+++ main.js
@@ -9,7 +9,103 @@ const  generateEl  = document.getElementById('generate');
 
 
 
-const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
-const numbers = "0123456789"
-const symbols = "!@$#%^&*()_=+"
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = "0123456789";
+const symbols = "!@$#%^&*()_=+";
+
+function getLowercase(){
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+console.log(getLowercase());
+
+function getUppercase(){
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+console.log(getUppercase());
+
+function getNumber(){
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+console.log(getNumber());
+
+
+function getSymbol(){
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(getSymbol());
+
+function generatePassword(){
+    const len = lenEl.value
+
+    let password = "" 
+
+    if(upperEl.checked){
+        password += getUppercase();
+    }
+    if(lowerEl.checked){
+        password += getLowercase();
+    }
+    if(numberEl.checked){
+        password += getNumber();
+    }
+    if(symbolEl.checked){
+        password += getSymbol();
+    }
+
+    for(let i = password.length; i < len; i++) {
+        const x = generateX();
+        password += x;
+    }
+    
+    pwEl.innerHTML = `${password}`;
+}
+
+function generateX(){
+    const xs = [];
+    if(upperEl.checked){
+        xs.push(getUppercase())
+    }
+    if(lowerEl.checked){
+        xs.push(getLowercase())
+    }
+    if(numberEl.checked){
+        xs.push(getNumber())
+    }
+    if(symbolEl.checked){
+        xs.push(getSymbol())
+    }
+    if(xs.length === 0){
+        resetLenght()
+        return ""
+       
+    }
+      
+    return xs[Math.floor(Math.random() * xs.length)];
+    
+}
+generateEl.addEventListener('click', () =>{
+    generatePassword()
+})
+
+function resetLenght(){
+    len.value = 1
+}
+
+copyEl.addEventListener('click',() => {
+    const textarea = document.createElement('textarea');
+    const password = pwEl.innerText;
+
+    if(!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('password copied to clipboard');
+
+})
+
