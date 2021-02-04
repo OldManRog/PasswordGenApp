558cba8ec54ad10dab85fd774d73a00a9ef00dfd
Rogerr Oliva <Roliva75@yahoo.com>
2021-02-03 19:35:19 -0600

    defined const elements and characters

---
+++ main.js
@@ -0,0 +1,15 @@
+const  pwEl = document.getElementById('pw');
+const  copyEl = document.getElementById('copy');
+const  lenEl= document.getElementById('len');
+const  upperEl = document.getElementById('upper');
+const  lowerEl = document.getElementById('lower');
+const  numberEl = document.getElementById('number');
+const  symbolEl = document.getElementById('symbol');
+const  generateEl  = document.getElementById('generate');
+
+
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+const numbers = "0123456789"
+const symbols = "!@$#%^&*()_=+"
